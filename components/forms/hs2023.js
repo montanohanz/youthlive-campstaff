52eb0eed4ad4ae9b2c99c082e66fccccd00a8576
Hanz Montano <montanohanz@gmail.com>
2023-03-29 14:13:20 +0800

    API Work

--- components/forms/hs2023.js
+++ components/forms/hs2023.js
@@ -34,56 +34,18 @@ export default class Hs2023Form extends React.Component {
   handleSubmit(event) {
     alert('A name was submitted: ' + this.state.name_first + ""  + this.state.name_last);
     event.preventDefault();
+    setSubmitting(true);
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          First Name:
-          <input name="name_first" type="text" value={this.state.name_first} onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Last Name:
-          <input name="name_last" type="text" value={this.state.name_last} onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Email:
-          <input name="email" type="text" value={this.state.email} onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Contact:
-          <input name="contact" type="text" value={this.state.contact} onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Birthday:
-          <input name="bday" type="text" value={this.state.bday} onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Emergency Contact Name:
-          <input name="em_name" type="text" value={this.state.em_name} onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Emergency Contact Number:
-          <input name="em_contact" type="text" value={this.state.em_contact} onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Relation:
-          <input name="em_relation" type="text" value={this.state.em_relation} onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Allergies:
-          <input name="" type="text" value={this.state.allergies} onChange={this.handleInputChange} />
-        </label>
-        <label>
-          YGroup Leader:
-          <input name="ygroup" type="text" value={this.state.ygroup} onChange={this.handleInputChange} />
-        </label>
-        <label>
-          Sponsor:
-          <input name="sponsor" type="text" value={this.state.sponsor} onChange={this.handleInputChange} />
-        </label>
-        <input type="submit" value="Submit" />
+        <fieldset>
+          <label>
+            <p>First Name</p>
+            <input name="name_first" />
+          </label>
+        </fieldset>
       </form>
     );
   }
