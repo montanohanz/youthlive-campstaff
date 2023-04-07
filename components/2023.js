939aff4cca3bac05e876f406dc2ed5906f4db2b8
Hanz Montano <montanohanz@gmail.com>
2023-04-08 03:47:31 +0800

    initial design phase

--- components/2023.js
+++ components/2023.js
@@ -1,3 +1,14 @@
+import bg2020 from '../public/SVG/fw_bg.svg';
+import word from '../public/SVG/word.svg';
+import bgGray from '../public/SVG/defaultBg.svg';
+import g1frame from '../public/SVG/g_frame1.svg';
+import g2frame from '../public/SVG/g_frame2.svg';
+import o1frame from '../public/SVG/o_frame1.svg';
+import o2frame from '../public/SVG/o_frame2.svg';
+import deetsbg from '../public/SVG/deetsbg.svg';
+import cl2023 from '../public/SVG/cl2023.svg';
+import faqimg from '../public/SVG/faq.svg';
+
 function Head(props) {
     return (
     <head>
@@ -12,87 +23,72 @@ function Head(props) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Greenhills Christian Fellowship - CampLIVE 2023" />
         <meta property="og:image" content="https://youthlive.gcf.org.ph/camplive2023/assets/banner-fb.jpg?v=5" />
-        <meta property="og:description" content="CampLIVE is the annual summer camp for high school and college students! You'll be spending four days with young people from all backgrounds while you encounter Jesus through action-packed games and activities, journey with others through Y-Groups, and have fellowship over God's message for the next generation." />
+        <meta property="og:description" content="CampLIVE is the annual summer camp for high school students! You'll be spending four days with young people from all backgrounds while you encounter Jesus through action-packed games and activities, journey with others through Y-Groups, and have fellowship over God's message for the next generation." />
         <meta property="twitter:card" content="summary_large_image">
         <meta proper="twitter:site" content="@gcfyouthlive">
         <meta name="twitter:creator" content="@gcfyouthlive">
         <meta name="twitter:title" content="Greenhills Christian Fellowship - CampLIVE 202">
-        <meta name="twitter:description" content="CampLIVE is the annual summer camp for high school and college students! You'll be spending four days with young people from all backgrounds while you encounter Jesus through action-packed games and activities, journey with others through Y-Groups, and have fellowship over God's message for the next generation.">
+        <meta name="twitter:description" content="CampLIVE is the annual summer camp for high schoolstudents! You'll be spending four days with young people from all backgrounds while you encounter Jesus through action-packed games and activities, journey with others through Y-Groups, and have fellowship over God's message for the next generation.">
         <meta name="twitter:image" content="https://youthlive.gcf.org.ph/camplive2023/img/banner-fb.jpg"> */}
     </head>
     );
 }
 
-function Fullwidth(props) {
+function BgDefault(props) {
     return (
-        <div className="container mx-auto text-center md:aspect-video" >
-            <h1 className="align-middle">full width camp logo & picture</h1>
+        <div className="">
+            <img className="" src={bgGray.src} />
         </div>
     );
 }
 
-// function Navbar(props) {
-//     return (
-//         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-//             <div className="container flex flex-wrap items-center justify-between mx-auto">
-//             <a href="https://flowbite.com/" className="flex items-center">
-//                 <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-//                 <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-//             </a>
-//             <div className="flex md:order-2">
-//                 <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
-//                 <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
-//                 <span className="sr-only">Open main menu</span>
-//                 <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-//             </button>
-//             </div>
-//             <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-//             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-//                 <li>
-//                 <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Item 1</a>
-//                 </li>
-//                 <li>
-//                 <a href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Item 2</a>
-//                 </li>
-//             </ul>
-//             </div>
-//         </nav>
-//     );
-// }
-
-function bgDefault(props) {
-    return (
-        <div className="">
-            <img src="">
-        </div>
-    )
+function Frames(props) {
+    if (props.type == "green1") {
+        return (<img className="" src={g1frame.src} />)
+    }
+    if (props.type == "green2") {
+        return (<img className="" src={g2frame.src} />)
+    }
+    if (props.type == "orange1") {
+        return (<img className="" src={o1frame.src} />)
+    }
+    if (props.type == "orange2") {
+        return (<img className="" src={o2frame.src} />)
+    }
 }
 
-function h1Green(props) {
-    return (
-        <div className="">
+function ImgSlider(props) {
 
-        </div>
-    )
 }
 
-function h1Orange(props) {
+function Fullwidth(props) {
     return (
-        <div className="">
-
+        <div className="fluid text-center relative" >
+            <img className="" src={bg2020.src} />
+            <img className="absolute scale-75" style={{top: '32%'}} src={word.src} />
+            <div className='absolute bottom-1/4 inset-x-0 text-2023-bondiblue xl:text-7xl md:text-5xl text-2xl'>July 6-19</div>
         </div>
-    )
+    );
 }
 
 function Deets(props) {
     return (
-        <div className="flex flex-row">
-            <div className="basis-1/2 bg-black"></div>
-            <div className="basis-1/2 px-1">
-                <p className="text-xl">Location:</p>
-                <p className="font-bold text-lg">CCT Tagaytay Retreat & Training Center</p><br />
-                <p className="text-xl">Date:</p>
-                <p className="font-bold text-lg">July 6-9, 2023</p>
+        <div className="relative">
+            <div className=''>
+                <img src={deetsbg.src} />
+            </div>
+            <div className='flex absolute inset-0 items-center'>
+                <img className="basis-1/2 grow h-48" src={cl2023.src} />
+                <div className='basis-1/2 grow'>
+                    <div className='rounded-lg bg-2023-ivory mb-2'>
+                        img slider 
+                    </div>
+                    <div className='rounded-lg bg-2023-ivory text-2023-bondiblue m-2 p-2'>
+                        is the annual summer camp for high school! You'll be spending four days with young people
+                        from all backgrounds while you encounter Jesus through action-packed games and activities, journey with others through
+                        Y-Groups, and have fellowship over God's message for the next generation.
+                    </div>
+                </div>
             </div>
         </div>
     );
@@ -100,33 +96,56 @@ function Deets(props) {
 
 function PersonGrid(props) {
     return (
-        <div>
-            <div className="flex flex-row justify-end">
-                <p className="font-serif text-6xl text-right">Speakers & Counselors</p>
+        <div className=''>
+            <div className='-z-10'>
+                <BgDefault />
             </div>
-            <div className="grid grid-cols-1 grid-rows-2 mx-auto text-center">
-                <div className="flex rounded-full shadow-md">speaker 1 picture</div>
-                <div className="columns-1 shadow-md">
-                    <p className="text-xl font-bold">Ptr. Rollyvic Tira</p>
-                    <p className="text-sm">Speaker</p>
-                    <p className="text-base">speaker 1 description</p>
+            <div className='flex columns-2xl'>
+                <div className='basis-1/2 relative'>
+                    <div className='absolute' style={{top: '10%', left: '2%', width: '20%'}}>
+                        <Frames type="orange1" /> 
+                    </div>
+                    <div className='absolute' style={{top: '15%', left: '30%', width: '20%'}}>
+                        <Frames type="orange2" /> 
+                    </div>
+                </div>
+                <div className='basis-1/2'>
+                    <h1>Camp Counselors</h1>
+                </div>
+            </div>
+
+            <div className='flex columns-2xl'>
+                <div className='basis-1/2 relative'>
+                    <div className='absolute' style={{bottom: '10%', right: '30%', width: '20%'}}>
+                        <Frames type="green1" /> 
+                    </div>
+                    <div className='absolute' style={{bottom: '15%', right: '2%', width: '20%'}}>
+                        <Frames type="green2" /> 
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
-{/* TODO: Make this a Dynamic Component */}
 
 function FAQ(props) {
     return (
-        <div />
+        <div>
+            <div className='-z-10'>
+                <BgDefault />
+            </div>
+            <div className='flex columns-2xl inset-0'>
+                <div className='basis-1/2'>faqtext</div>
+                <img className='basis-1/2 scale-25' src={faqimg.src} />
+            </div>
+        </div>
     );
 }
 
 function CTA(props) {
     return (
         <div className="container mx-auto text-center">
-            <div className="container" name="banner">
+            <div className="container">
                 <div className="shadow-md">
                     <p className="text-xl">call to action</p>
                     <p className="text-base">button</p>
@@ -150,31 +169,9 @@ function Footer(props) {
                 </div>
             </div>
         </div>
-        <script src=".\assets\js\jquery.min.js"></script>
-        <script src=".\assets\js\popper.min.js"></script>
-        {/* <script>
-            $(document).ready(function(){
-              var $root = $('html, body');
-          
-              $('a[href^="#"]').click(function () {
-                $root.animate({
-                  scrollTop: $( $.attr(this, 'href') ).offset().top
-                }, 500);
-                return false;
-              });
-          
-              $('.answer').on('show.bs.collapse', function() {
-                $(this).parents('.question').siblings('.fa-caret-right fa-solid').css({'transform':'rotate(90deg)','color':'#F06B4B'});
-                $(this).siblings('a').css('color', '#F06B4B');
-              });
-              $('.answer').on('hide.bs.collapse', function() {
-                $(this).parents('.question').siblings('.fa-caret-right fa-solid').css({'transform':'none','color':'#000000'});
-                $(this).siblings('a').css('color', '#1a1a1a');
-              });
-            });
-            </script> */}
-            <script src="https://kit.fontawesome.com/abc2906f89.js" ork="anonymous"></script>
-        <script src="" async defer></script>
+        {/* <script src=".\assets\js\jquery.min.js"></script> */}
+        {/* <script src=".\assets\js\popper.min.js"></script> */}
+        {/* <script src="https://kit.fontawesome.com/abc2906f89.js" ork="anonymous"></script> */}
         </footer>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      '2023-moonstone':'#35BBCA',
+      '2023-bondiblue': '#0991B4',
+      '2023-ivory': '#FFFCEB',
+      '2023-sbyellow': '#F8D90F',
+      '2023-pear': '#C0CB00',
+      '2023-safetyorange': '#FE7A15',
+    },
+    fontFamily: {
+      sans: ['DM Sans', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
